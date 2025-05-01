412e3eac49aa54c4ca578a455f097495101ba09f
myacc0 <aay0994@gmail.com>
2025-05-01 14:17:12 +0500

    Lesson8, sync changes

---
+++ src/ru/javawebinar/basejava/storage/AbstractFileStorage.java
@@ -0,0 +1,76 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.exception.StorageException;
+import ru.javawebinar.basejava.model.Resume;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+public abstract class AbstractFileStorage extends AbstractStorage<File> {
+    private final File directory;
+
+    protected AbstractFileStorage(File directory) {
+        Objects.requireNonNull(directory, "directory must not be null");
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not directory");
+        }
+        if (!directory.canRead() || !directory.canWrite()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not readable/writable");
+        }
+        this.directory = directory;
+    }
+
+    @Override
+    protected void processUpdate(File file, Resume r) {
+
+    }
+
+    @Override
+    protected void processSave(File file, Resume r) {
+        try {
+            file.createNewFile();
+            doWrite(r, file);
+        } catch (IOException e) {
+            throw new StorageException("IO error", file.getName(), e);
+        }
+    }
+
+    @Override
+    protected Resume processGet(File file) {
+        return null;
+    }
+
+    @Override
+    protected void processDelete(File file) {
+
+    }
+
+    @Override
+    protected File getSearchKey(String uuid) {
+        return new File(directory, uuid);
+    }
+
+    @Override
+    protected boolean isExist(File file) {
+        return file.exists();
+    }
+
+    @Override
+    protected List<Resume> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    protected abstract void doWrite(Resume r, File file) throws IOException;
+}
