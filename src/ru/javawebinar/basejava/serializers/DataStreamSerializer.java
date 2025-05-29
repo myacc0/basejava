dab91e0e72af382ff6f82edaec3d1d936b986f25
myacc0 <aay0994@gmail.com>
2025-05-29 11:34:50 +0500

    HW10, DataStreamSerializer code refactoring, using lambda

--- src/ru/javawebinar/basejava/serializers/DataStreamSerializer.java
+++ src/ru/javawebinar/basejava/serializers/DataStreamSerializer.java
@@ -4,8 +4,10 @@ import ru.javawebinar.basejava.model.*;
 import ru.javawebinar.basejava.util.DateUtil;
 
 import java.io.*;
-import java.util.List;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class DataStreamSerializer implements ResumeSerializer {
 
@@ -14,21 +16,25 @@ public class DataStreamSerializer implements ResumeSerializer {
         try (DataOutputStream dos = new DataOutputStream(os)) {
             dos.writeUTF(r.getUuid());
             dos.writeUTF(r.getFullName());
-            Map<ContactType, String> contacts = r.getContacts();
-            dos.writeInt(contacts.size());
-            for (Map.Entry<ContactType, String> e: contacts.entrySet()) {
+
+            writeCollection(dos, r.getContacts().entrySet(), (e) -> {
                 dos.writeUTF(e.getKey().name());
                 dos.writeUTF(e.getValue());
-            }
+            });
 
             dos.writeUTF(((TextSection) r.getSections().get(SectionType.OBJECTIVE)).getContent());
             dos.writeUTF(((TextSection) r.getSections().get(SectionType.PERSONAL)).getContent());
 
-            writeListSection((ListSection) r.getSections().get(SectionType.ACHIEVEMENT), dos);
-            writeListSection((ListSection) r.getSections().get(SectionType.QUALIFICATIONS), dos);
+            writeCollection(dos, ((ListSection) r.getSections().get(SectionType.ACHIEVEMENT)).getContent(), dos::writeUTF);
+            writeCollection(dos, ((ListSection) r.getSections().get(SectionType.QUALIFICATIONS)).getContent(), dos::writeUTF);
 
-            writeOrganizationSection((OrganizationSection) r.getSections().get(SectionType.EXPERIENCE), dos);
-            writeOrganizationSection((OrganizationSection) r.getSections().get(SectionType.EDUCATION), dos);
+            writeCollection(dos,
+                    ((OrganizationSection) r.getSections().get(SectionType.EXPERIENCE)).getContent(),
+                    o -> writeOrganization(dos, o));
+
+            writeCollection(dos,
+                    ((OrganizationSection) r.getSections().get(SectionType.EDUCATION)).getContent(),
+                    o -> writeOrganization(dos, o));
         }
     }
 
@@ -37,76 +43,74 @@ public class DataStreamSerializer implements ResumeSerializer {
         try (DataInputStream dis = new DataInputStream(is)) {
             String uuid = dis.readUTF();
             String fullName = dis.readUTF();
-            Resume resume = new Resume(uuid, fullName);
-            int contactsSize = dis.readInt();
-            for (int i = 0 ; i < contactsSize; i++) {
-                resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
-            }
+            Resume r = new Resume(uuid, fullName);
 
-            resume.getSections().put(SectionType.OBJECTIVE, new TextSection(dis.readUTF()));
-            resume.getSections().put(SectionType.PERSONAL, new TextSection(dis.readUTF()));
+            Set<Map.Entry<ContactType, String>> contactsEntrySet = new HashSet<>();
+            readCollection(dis, contactsEntrySet, d -> Map.entry(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
+            contactsEntrySet.forEach(entry -> r.addContact(entry.getKey(), entry.getValue()));
 
-            resume.getSections().put(SectionType.ACHIEVEMENT, readListSection(dis));
-            resume.getSections().put(SectionType.QUALIFICATIONS, readListSection(dis));
+            r.addSection(SectionType.OBJECTIVE, new TextSection(dis.readUTF()));
+            r.addSection(SectionType.PERSONAL, new TextSection(dis.readUTF()));
 
-            resume.getSections().put(SectionType.EXPERIENCE, readOrganizationSection(dis));
-            resume.getSections().put(SectionType.EDUCATION, readOrganizationSection(dis));
-            return resume;
+            r.addSection(SectionType.ACHIEVEMENT, new ListSection());
+            readCollection(dis, ((ListSection) r.getSections().get(SectionType.ACHIEVEMENT)).getContent(), DataInput::readUTF);
+
+            r.addSection(SectionType.QUALIFICATIONS, new ListSection());
+            readCollection(dis, ((ListSection) r.getSections().get(SectionType.QUALIFICATIONS)).getContent(), DataInput::readUTF);
+
+            r.addSection(SectionType.EXPERIENCE, new OrganizationSection());
+            readCollection(dis, ((OrganizationSection) r.getSections().get(SectionType.EXPERIENCE)).getContent(), this::readOrganization);
+
+            r.addSection(SectionType.EDUCATION, new OrganizationSection());
+            readCollection(dis, ((OrganizationSection) r.getSections().get(SectionType.EDUCATION)).getContent(), this::readOrganization);
+            return r;
         }
     }
 
-    private void writeListSection(ListSection section, DataOutputStream dos) throws IOException {
-        List<String> items = section.getContent();
-        dos.writeInt(items.size());
-        for (String i: items) {
-            dos.writeUTF(i);
+    private interface CollectionItemWriter<T> {
+        void write(T item) throws IOException;
+    }
+
+    private interface CollectionItemReader<T> {
+        T read(DataInputStream dis) throws IOException;
+    }
+
+    private <T> void writeCollection(DataOutputStream dos, Collection<T> c, CollectionItemWriter<T> writer) throws IOException {
+        dos.writeInt(c.size());
+        for (T item : c) {
+            writer.write(item);
         }
     }
 
-    private ListSection readListSection(DataInputStream dis) throws IOException {
+    private <T> void readCollection(DataInputStream dis, Collection<T> c, CollectionItemReader<T> reader) throws IOException {
         int itemsSize = dis.readInt();
-        ListSection section = new ListSection();
-        for (int i = 0 ; i < itemsSize; i++) {
-            section.getContent().add(dis.readUTF());
-        }
-        return section;
-    }
-
-    private void writeOrganizationSection(OrganizationSection section, DataOutputStream dos) throws IOException {
-        List<Organization> items = section.getContent();
-        dos.writeInt(items.size());
-        for (Organization i: items) {
-            dos.writeUTF(i.getName());
-            dos.writeUTF(i.getWebsite());
-
-            dos.writeInt(i.getPeriods().size());
-            for (Period p : i.getPeriods()) {
-                dos.writeUTF(DateUtil.format(p.getStartDate()));
-                dos.writeUTF(DateUtil.format(p.getEndDate()));
-                dos.writeUTF(p.getDescription());
-            }
+        for (int i = 0; i < itemsSize; i++) {
+            c.add(reader.read(dis));
         }
     }
 
-    private OrganizationSection readOrganizationSection(DataInputStream dis) throws IOException {
-        int itemsSize = dis.readInt();
-        OrganizationSection section = new OrganizationSection();
-        for (int i = 0 ; i < itemsSize; i++) {
-            Organization o = new Organization();
-            o.setName(dis.readUTF());
-            o.setWebsite(dis.readUTF());
+    private void writeOrganization(DataOutputStream dos, Organization o) throws IOException {
+        dos.writeUTF(o.getName());
+        dos.writeUTF(o.getWebsite());
+        writeCollection(dos, o.getPeriods(), p -> {
+            dos.writeUTF(DateUtil.format(p.getStartDate()));
+            dos.writeUTF(DateUtil.format(p.getEndDate()));
+            dos.writeUTF(p.getDescription());
+        });
+    }
 
-            int periodsSize = dis.readInt();
-            for (int j = 0; j < periodsSize; j++) {
-                Period p = new Period();
-                p.setStartDate(DateUtil.parse(dis.readUTF()));
-                p.setEndDate(DateUtil.parse(dis.readUTF()));
-                p.setDescription(dis.readUTF());
-                o.getPeriods().add(p);
-            }
-            section.getContent().add(o);
-        }
-        return section;
+    private Organization readOrganization(DataInputStream dis) throws IOException {
+        Organization o = new Organization();
+        o.setName(dis.readUTF());
+        o.setWebsite(dis.readUTF());
+        readCollection(dis, o.getPeriods(), d -> {
+            Period p = new Period();
+            p.setStartDate(DateUtil.parse(dis.readUTF()));
+            p.setEndDate(DateUtil.parse(dis.readUTF()));
+            p.setDescription(dis.readUTF());
+            return p;
+        });
+        return o;
     }
 
 }
